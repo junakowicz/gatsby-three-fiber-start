f981654554171d968150aaf24fe88dc9add0f2a7
junakowicz <andrzej@bitomer.com>
2019-12-07 21:37:46 +0000

    extract main

---
+++ src/components/main.js
@@ -0,0 +1,77 @@
+import * as THREE from "three"
+import React from "react"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+import { Canvas } from "react-three-fiber"
+import Model from "../components/model"
+import Controls from "../components/controls"
+import GLTF_BUC from "./BUCv2.gltf"
+import uniforms from "./uniforms"
+import "../style.css"
+
+uniforms.init(THREE)
+
+const RectAreaLightDecl = ({
+  color = "white",
+  intensity = 1.5,
+  width = 1000,
+  height = 400,
+  position = [100, 200, -700],
+  lookAt = [0, 0, 0],
+}) => {
+  return (
+    <rectAreaLight
+      intensity={intensity}
+      position={position}
+      color={color}
+      width={width}
+      height={height}
+      onUpdate={self => self.lookAt(...lookAt)}
+    />
+  )
+}
+
+
+function Main() {
+ return (<main>
+    <div style={{ position: 'absolute', color: 'white', left: 9, top: 9 }}>HORNBEAM BICYCLES</div>
+    <Canvas camera={{ position: [0, 0, 300] }}>
+      <ambientLight intensity={0.2} />
+      <RectAreaLightDecl />
+      <RectAreaLightDecl
+        intensity={4}
+        width={100}
+        height={1000}
+        position={[0, 0, 2000]}
+        color="purple"
+      />
+      <RectAreaLightDecl
+        intensity={0.5}
+        width={500}
+        height={1000}
+        position={[0, 1000, 0]}
+      />
+
+      <RectAreaLightDecl
+        intensity={5}
+        width={1000}
+        height={100}
+        position={[-800, 0, 800]}
+      />
+      <Model url={GLTF_BUC} />
+      <Controls
+        autoRotate
+        autoRotateSpeed={0.5}
+        enablePan={false}
+        enableZoom={false}
+        enableDamping
+        dampingFactor={0.5}
+        rotateSpeed={1}
+      // maxPolarAngle={Math.PI / 3}
+      // minPolarAngle={Math.PI / 3}
+      />
+    </Canvas>
+  </main>)
+}
+
+export default Main
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,14 @@
-import * as THREE from "three"
-import React from "react"
-import { Canvas } from "react-three-fiber"
-import Model from "../components/model"
-import Controls from "../components/controls"
-// import GLTF from "../components/gatsby.gltf"
-// import GLTF_BUC from "../components/gatsby.gltf"
-import GLTF_BUC from "../components/BUCv2.gltf"
-import uniforms from "../components/uniforms"
 import "../style.css"
+import Main from "../components/main";
+import React from "react"
 
-uniforms.init(THREE)
 
-const RectAreaLightDecl = ({
-  color = "white",
-  intensity = 1.5,
-  width = 1000,
-  height = 400,
-  position = [100, 200, -700],
-  lookAt = [0, 0, 0],
-}) => {
+export default () => {
+  
+  // if (typeof window !== `undefined`) return null
+
   return (
-    <rectAreaLight
-      intensity={intensity}
-      position={position}
-      color={color}
-      width={width}
-      height={height}
-      onUpdate={self => self.lookAt(...lookAt)}
-    />
+   <Main/> 
   )
 }
 
-export default () => (
-  <main>
-  <div style={{position:'absolute', color:'white', left:9,top:9}}>HORNBEAM BICYCLES</div>
-    <Canvas camera={{ position: [0, 0, 300] }}>
-      <ambientLight intensity={0.2} />
-      <RectAreaLightDecl />
-      <RectAreaLightDecl
-        intensity={4}
-        width={100}
-        height={1000}
-        position={[0, 0, 2000]}
-        color="purple"
-      />
-      <RectAreaLightDecl
-        intensity={0.5}
-        width={500}
-        height={1000}
-        position={[0, 1000, 0]}
-      />
-
-      <RectAreaLightDecl
-        intensity={5}
-        width={1000}
-        height={100}
-        position={[-800, 0, 800]}
-      />
-      <Model url={GLTF_BUC}/>
-      <Controls
-        autoRotate
-        autoRotateSpeed={0.5}
-        enablePan={false}
-        enableZoom={false}
-        enableDamping
-        dampingFactor={0.5}
-        rotateSpeed={1}
-        // maxPolarAngle={Math.PI / 3}
-        // minPolarAngle={Math.PI / 3}
-      />
-    </Canvas>
-  </main>
-)
