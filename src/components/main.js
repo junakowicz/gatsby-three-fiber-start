ff3feeebe07320229a830899a867074cadb73b88
junakowicz <andrzej@bitomer.com>
2020-02-22 13:40:38 +0000

    adds fabric

--- src/components/main.js
+++ src/components/main.js
@@ -3,8 +3,10 @@ import React from "react"
 import { Canvas } from "react-three-fiber"
 import Model from "../components/model"
 import Controls from "../components/controls"
-import GLTF_BUC from "./BUCv2.gltf"
+// import GLTF_BUC from "./BUCv2.gltf"
+import GLTF_BUC from "./buc3blenderop.gltf"
 import uniforms from "./uniforms"
+import { fabric } from "fabric";
 import "../style.css"
 
 uniforms.init(THREE)
@@ -29,8 +31,114 @@ const RectAreaLightDecl = ({
   )
 }
 
+function createTextureCanvas() {
+  // var x = document.createElement("TCANVAS");
+  // console.log("DOCCCCxxx",x)
+  // var ctx = x.getContext("2d");
+  // ctx.fillStyle = "#FF0000";
+  // ctx.fillRect(20, 20, 150, 100);
+  // document.body.appendChild(x);
+  var canvas = document.createElement('canvas');
+
+  canvas.id = "tCanvas";
+
+  canvas.style.width = '400px';
+  canvas.style.height = '400px';
+  canvas.style.zIndex = 8;
+  canvas.style.position = "absolute";
+  canvas.style.top = 0;
+  canvas.style.border = "1px solid";
+
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(canvas);
+
+console.log('fabricc', fabric)
+
+var fCanvas = new fabric.Canvas('tCanvas', {
+  backgroundColor: 'transparent',
+   width: 400, height: 400,
+   position: 'absolute',
+   top:0, 
+});
+
+const fCanvasContainer = fCanvas.getElement().parentNode;
+fCanvasContainer.style.position = 'absolute'
+
+
+var text = new fabric.IText('Three.js\n+\nFaBric.js', {
+  fontSize: 40,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  left: 128,
+  top: 128,
+  angle: 30,
+  originX: 'center',
+  originY: 'center',
+  shadow: 'blue -5px 6px 5px',
+  styles: {
+    0: {
+      0: {
+        fontSize: 60,
+        fontFamily: 'Impact',
+        fontWeight: 'normal',
+        fill: 'orange'
+      }
+    },
+    1: {
+      0: {
+        fill: "blue"
+      }
+    },
+    2: {
+      0: {
+        textBackgroundColor: 'red'
+      },
+      2: {
+        fill: 'fuchsia',
+        stroke: 'orange',
+        strokeWidth: 1
+      }
+    }
+  }
+});
+text.setSelectionStyles({
+  fontStyle: 'italic',
+  fill: '',
+  stroke: 'red',
+  strokeWidth: 2
+}, 1, 5);
+fCanvas.add(text);
+fCanvas.setActiveObject(text);
+
+var rect = new fabric.Rect({
+  width: 50,
+  height: 50,
+  left: 50,
+  top: 128,
+  stroke: '#aaf',
+  strokeWidth: 5,
+  fill: '#faa',
+  selectable: false,
+  originX: 'center',
+  originY: 'center'
+});
+fCanvas.add(rect);
+
+// below is optional
+
+// var ctx = tCanvas.getContext("2d");
+// ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
+// ctx.fillRect(100, 100, 200, 200);
+// ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
+// ctx.fillRect(150, 150, 200, 200);
+// ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+// ctx.fillRect(200, 50, 200, 200);
+}
 
 function Main() {
+  createTextureCanvas()
+
  return (<main>
     <div style={{ position: 'absolute', color: 'white', left: 9, top: 9 }}>HORNBEAM BICYCLES</div>
     <Canvas camera={{ position: [0, 0, 300] }}>
--- src/components/model.js
+++ src/components/model.js
@@ -5,9 +5,83 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 function Model({ url }) {
   const [scene, set] = React.useState()
 
+  // var PIXEL_RATIO = (function () {
+  //   var ctx = document.createElement('canvas').getContext('2d'),
+  //     dpr = window.devicePixelRatio || 1,
+  //     bsr = ctx.webkitBackingStorePixelRatio ||
+  //       ctx.mozBackingStorePixelRatio ||
+  //       ctx.msBackingStorePixelRatio ||
+  //       ctx.oBackingStorePixelRatio ||
+  //       ctx.backingStorePixelRatio || 1;
+  //   return dpr / bsr;
+  // })();
+  
+  // const createRetinaCanvas = function (w, h, ratio) {
+  //   if (!ratio) { ratio = PIXEL_RATIO; }
+  //   var can = document.createElement('canvas');
+  //   can.width = w * ratio;
+  //   can.height = h * ratio;
+  //   can.style.width = w + 'px';
+  //   can.style.height = h + 'px';
+  //   can.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
+  //   return can;
+  // }
+  // function addTexture() {
+  //   var text = 'cats'
+  //   //create image
+  //   var bitmap = createRetinaCanvas(100, 100);
+  //   var ctx = bitmap.getContext('2d', {antialias: false});
+  //   ctx.font = 'Bold 20px Arial';
+  //   ctx.beginPath();
+  //   ctx.rect(0, 0, 100, 100);
+  //   ctx.fillStyle = 'red';
+  //   ctx.fill();
+  //   ctx.fillStyle = 'white';
+  //   ctx.fillText(text, 0, 20);
+  //   ctx.strokeStyle = 'black';
+  //   ctx.strokeText(text, 0, 20);
+  //   // canvas contents will be used for a texture
+  //   var texture = new THREE.Texture(bitmap) 
+  //   texture.needsUpdate = true;
+  //   var material = new THREE.MeshBasicMaterial({ map: texture });
+  //   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  //   cube = new THREE.Mesh(geometry, material);
+  //   scene.add(cube)
+  // }
+
+
+
   React.useMemo(
-    () =>
-      new GLTFLoader().load(url, gltf => {
+    () => {
+
+// canvas texture
+      // var text = 'cats'
+      // //create image
+      // var bitmap = createRetinaCanvas(300, 300);
+      // var ctx = bitmap.getContext('2d', { antialias: false });
+      // ctx.font = 'Bold 150px Arial';
+      // ctx.beginPath();
+      // ctx.rect(0, 0, 300, 300);
+      // ctx.fillStyle = 'red';
+      // ctx.fill();
+      // ctx.fillStyle = 'white';
+      // ctx.fillText(text, 0, 20);
+      // ctx.strokeStyle = 'black';
+      // ctx.strokeText(text, 0, 20);
+      // // canvas contents will be used for a texture
+      // var texture = new THREE.Texture(bitmap)
+      // texture.needsUpdate = true;
+      // var material = new THREE.MeshBasicMaterial({ map: texture });
+
+
+
+
+
+
+
+
+
+      return new GLTFLoader().load(url, gltf => {
         gltf.scene.traverse(obj => {
           if (obj.type === "Mesh") {
             obj.material.dispose()
@@ -21,7 +95,8 @@ function Model({ url }) {
         })
         set(gltf.scene)
       }),
-    [url]
+        [url]
+    }
   )
 
   return scene ? <primitive object={scene} rotation={[-1.5707963267948963, 0, 0]} /> : null
